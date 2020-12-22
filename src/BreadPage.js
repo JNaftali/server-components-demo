6f1f2a7a17ad64a358a6c78e2647763046470fe4
JNaftali <jmarantz@thelabnyc.com>
2020-12-22 15:29:12 -0500

    bread pages

--- src/BreadPage.js
+++ src/BreadPage.js
@@ -2,6 +2,7 @@ export default function BreadPage({page}) {
   return (
     <>
       <h1>{page.title}</h1>
+      <div>{page.introduction}</div>
     </>
   );
 }
--- src/wagtail.config.js
+++ src/wagtail.config.js
@@ -1,11 +1,13 @@
 // @ts-check
 import HomePage from './HomePage';
 import BreadsIndexPage from './BreadsIndexPage';
+import BreadPage from './BreadPage';
 
 export const wagtailConfig = {
   domain: 'http://localhost:8000',
   pageTypes: {
     'base.HomePage': HomePage,
     'breads.BreadsIndexPage': BreadsIndexPage,
+    'breads.BreadPage': BreadPage,
   },
 };
