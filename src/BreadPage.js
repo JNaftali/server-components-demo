6be09386f1daaecc8831dda95756202d83d10c14
JNaftali <jmarantz@thelabnyc.com>
2020-12-24 14:53:00 -0500

    add picture

--- src/BreadPage.js
+++ src/BreadPage.js
@@ -3,6 +3,12 @@ export default function BreadPage({page}) {
     <>
       <h1>{page.title}</h1>
       <div>{page.introduction}</div>
+      <img
+        height="375"
+        width="500"
+        src={`http://localhost:8000${page.image.meta.download_url}`}
+        alt={page.image.title}
+      />
     </>
   );
 }
